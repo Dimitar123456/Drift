d4c5d67cfd7f7c3839f6a5121883fada1232e127
mikaelakostova15 <113662876+mikaelakostova15@users.noreply.github.com>
2023-01-12 13:19:26 +0200

    zadachi

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            int idk = 1;
+            Console.WriteLine("otpechatva chislata ot 1 do N prez 3");
+            for (var i = 0; i >= n; i = 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
